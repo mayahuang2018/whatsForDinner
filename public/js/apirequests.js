96d582930a9ca60ed9e4e9730439587a98d08a6d
Hilary <hilary.scott90@gmail.com>
2019-06-14 23:00:33 -0400

    retrieved api data

--- public/js/apirequests.js
+++ public/js/apirequests.js
@@ -1,36 +1,52 @@
 function recipes(ingredients) {
 
   // var ingredients = $("#search").val();
-  console.log('get recipes')
+  // console.log('get recipes')
   var queryURL = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + ingredients + "&p=5"
   $.ajax({
     url: queryURL,
     method: "GET",
     dataType: 'json'
   })
-  .then(function (response) {
-    console.log('hello')
-    // Printing the entire object to console
-    console.log(response.results);
+    .then(function (response) {
+      // Printing the entire object to console
+      console.log(response.results);
+      var results = response.results;
 
-    response.results.map(function(ele) {console.log(ele.title)});
-    response.results.map(function(ele) {console.log(ele.href)});
-    response.results.map(function(ele) {console.log(ele.ingredients)})
+      var title = results.map(function (ele) {
+        console.log(ele.title);
+        return results.title;
+      });
+
+      var href = results.map(function (ele) {
+        console.log(ele.href);
+        return results.href;
+      });
+
+      var ingredients = results.map(function (ele) {
+        console.log(ele.ingredients);
+        return results.ingredients;
+      });
+
+      var thumbnail = results.map(function (ele) {
+        console.log(ele.thumbnail);
+        return results.thumbnail;
+      });
 
 
-    // Constructing HTML containing the recipe information
-    // var recipeName = $("<h1>").text(response.title);
-    // var recipeLink = $("<a>").attr("href", response.href);
-    // var recipeImage = $("<img>").attr("src", response.thumbnail);
-    // var recipeIngredients = $("<h2>").text(response.ingredients);
+      // Constructing HTML containing the recipe information
+      // var recipeName = $("<h1>").text(response.title);
+      // var recipeLink = $("<a>").attr("href", response.href);
+      // var recipeImage = $("<img>").attr("src", response.thumbnail);
+      // var recipeIngredients = $("<h2>").text(response.ingredients);
 
-    // append the new recipe content
+      // append the new recipe content
 
-    $("#recipe-div").append(recipeName, recipeLink, recipeImage, recipeIngredients);
-  })
+      // $("#recipe-div").append(recipeName, recipeLink, recipeImage, recipeIngredients);
+    })
 }
 
-//  //need to swap out the eyeglass icon image
+//need to swap out the eyeglass icon image
 $(document).on("click", '#search', function (event) {
   // Preventing the button from trying to submit the form
   event.preventDefault();
@@ -39,4 +55,5 @@ $(document).on("click", '#search', function (event) {
   console.log(inputRecipe)
   // Running the function(passing in the recipe as an argument)
   recipes(inputRecipe);
+
 });
