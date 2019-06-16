d5b6b6751e5f58319ec46102bf55d59bb8780c63
Maya Chen <fangnihuang2016@gmail.com>
2019-06-16 17:52:55 -0400

    input enter

--- public/js/apirequests.js
+++ public/js/apirequests.js
@@ -1,37 +1,36 @@
 // const recipe = require("../models/apiResponse");
 // const db = require("../models");
+$(document).ready(function () {
 
+  function recipes(ingredients) {
+    $(".search-results").empty();
+    // var ingredients = $("#search").val();
+    // console.log('get recipes')
+    var queryURL = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + ingredients + "&p=5";
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      dataType: 'json'
+    })
+      .then(function (response) {
+        // Printing the entire object to console
+        console.log(response.results);
+        var results = response.results;
 
-function recipes(ingredients) {
-  $(".search-results").empty();
+        const data = {
+          title: results.title,
+          link: results.href,
+          ingredients: results.ingredients,
+          thumbnail: results.thumbnail
+        };
+        console.log(data);
 
-  // var ingredients = $("#search").val();
-  // console.log('get recipes')
-  var queryURL = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + ingredients + "&p=5";
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    dataType: 'json'
-  })
-    .then(function (response) {
-      // Printing the entire object to console
-      console.log(response.results);
-      var results = response.results;
+        results.forEach(data => {
+          console.log('Result here', data)
 
-      const data = {
-        title: results.title,
-        link: results.href,
-        ingredients: results.ingredients,
-        thumbnail: results.thumbnail
-      };
-      console.log(data);
+          $("#addRec").on("click", clickFavBorder)
 
-      results.forEach(data => {
-      console.log('Result here', data)
-
-      $("#addRec").on("click",clickFavBorder)
-  
-      $(".search-results").append(`
+          $(".search-results").append(`
    <div>
     <div class="col s12 m5" id="recipeForm">
       <div class="card z-depth-5">
@@ -50,52 +49,55 @@ function recipes(ingredients) {
     </div>
   </div>
         `
-        
-        )
 
+          )
+
+
+        })
 
       })
 
-    })
 
-   
-}
+  }
 
-// 
-function clickFavBorder () {
-  console.log("heart clicked!")
-  var stored = $(this).attr("data-stored");
+  // change color of "heart" button when it's clicked
+  function clickFavBorder() {
+    console.log("heart clicked!")
+    var stored = $(this).attr("data-stored");
 
-  if (stored == "pink"){
+    if (stored == "pink") {
       $(this).attr("class", $(this).data("green"));
       $(this).attr("data-stored", "green");
-        } else{
-            $(this).attr("class", $(this).data("pink"));
-            $(this).attr("data-stored", "pink");  
-        }  
-   
-}
+    } else {
+      $(this).attr("class", $(this).data("pink"));
+      $(this).attr("data-stored", "pink");
+    }
 
+  }
 
+ 
 
+//allows the user to use the 'enter' button on their keyboard to also get the recipe results
+  var input = document.getElementById("recipe-input");
+  input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     document.getElementById("search").click();
+    }
 
-//need to swap out the eyeglass icon image
-$(document).on("click", '#search', function (event) {
-  // Preventing the button from trying to submit the form
-  event.preventDefault();
-  // Storing the recipe name
-  var inputRecipe = $("#recipe-input").val().trim();
-  console.log(inputRecipe)
-  // Running the function(passing in the recipe as an argument)
-  recipes(inputRecipe);
+  });
 
+  //need to swap out the eyeglass icon image
+  $('#search').on("click", function (event) {
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+    // Storing the recipe name
+    var inputRecipe = $("#recipe-input").val().trim();
+    console.log(inputRecipe)
+    // Running the function(passing in the recipe as an argument)
+    recipes(inputRecipe);
+
+  });
+
+ 
 });
-
-// var input = document.getElementById("recipe-input");
-// input.addEventListener("keyup", function(event) {
-//   if (event.keyCode === 13) {
-//    event.preventDefault();
-//    document.getElementById("search").click();
-//   }
-
-// });
